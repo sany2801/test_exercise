8d29115955a67ac3f6d631ee6f436efe845fb704
Александр <sasha82018201@gmail.com>
2021-12-08 14:33:52 +0300

    finishd

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-const inputFilter = document.querySelector('.input-filter')
-const filterValue = [...document.querySelectorAll('.input-filter_value')]
-console.log(filterValue)
+const close = document.querySelector('.close')
+const popap = document.querySelector('.wrapperpopap')
+const adjust = document.querySelector('.adjust')
+const menuButton = document.querySelector('.menu-button')
+const mobileMenu = document.querySelector('.mobile-menu')
+const checkboxoff = document.querySelector('.checkboxoff')
+
+// menuButton.addEventListener('click', () =>{
+//     mobileMenu.style.display = "block"
+// })
+adjust.addEventListener('click', () =>{
+    popap.style.display = "block"
+})
+close.addEventListener('click', () =>{
+    popap.style.display = "none"
+})
+
+
+const checkbox = () => {
+    menuButton.addEventListener('click', () =>{
+        if(menuButton.checked === true){
+         mobileMenu.style.display = "block"
+        }else{
+         mobileMenu.style.display = "none"
+        }
+    })
+    }
+
+checkbox()
